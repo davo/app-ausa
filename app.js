10ba8f005cfcc243d5f2697cd009794c72087209
luis <luisuranga@buenosaires.gob.ar>
2015-03-10 14:27:43 -0300

    idento

--- app.js
+++ app.js
@@ -51,60 +51,63 @@ var TRAFICO = {
 	"FF0000":"trafico_congestionado"
 };
 
+var host = 'http://www.ausa.com.ar/autopista/carteleria/plano/mime.txt'
+
 io.sockets.on('connection', function(socket){
 
-	setInterval(function(){
+setInterval(function(){
 
-		request({uri: 'http://www.ausa.com.ar/autopista/carteleria/plano/mime.txt'}, function(err, resp, data){
+	request({uri: host}, function(err, resp, data){
 
-			if (!err){
-				console.log("cargando..");
-				console.log('received data: ok');
-				var a = data.split('&');
-				_.each(a, function(x){
-					if (x.match(patron)) {
-						lista.push(x);
-					}
-				});
-			} else {
-				console.log(err);
+	if (!err){
+		console.log("cargando..");
+		console.log('received data: ok');
+		var a = data.split('&');
+		_.each(a, function(x){
+			if (x.match(patron)) {
+				lista.push(x);
 			}
-			
-			function prueba(n) {
-				for (var key in TRAFICO){
-					if (key.match(n.split("=")[1])) {
-						dictionary[n] = TRAFICO[key];
-					}
-				}
-			}
-
-			_.filter(lista, prueba);
-		
-			var test = [];
-			var rr = {};
-			var alerts = {"trafico_fluido": "active", "trafico_lento": "success", 
-				"trafico_con_demoras": "warning", "trafico_congestionado": "danger"};
-
-			for (var key in dictionary){
-				for (var r = 0; r<vpm.length;r++){
-					if (key.split("=")[0] == vpm[r].vpm){
-						test.push({
-							"ubicacion" : vpm[r].ubicacion.replace(/[:_]/g, " ").toUpperCase(),
-							"estado" : dictionary[key].replace(/_/g, " ").toUpperCase(),
-							"alert" : alerts[dictionary[key]]
-						})
-						// TestSave = new db({
-						// 	"ubicacion":  vpm[r].ubicacion,
-						// 	"estado": dictionary[key],
-						// }).save()
-					}
-				}
-			}
+		});
+	} else {
+		console.log(err);
+	}
 	
-			socket.emit('msg', {'msg': test});
-			socket.emit('msgWrite', msgWrite);
-		})
+	function prueba(n) {
+		for (var key in TRAFICO){
+			if (key.match(n.split("=")[1])) {
+				dictionary[n] = TRAFICO[key];
+			}
+		}
+	}
 
-	}, 15000);
+	_.filter(lista, prueba);
+
+	var test = [];
+	var rr = {};
+	var alerts = {"trafico_fluido": "active", "trafico_lento": "success", 
+		"trafico_con_demoras": "warning", "trafico_congestionado": "danger"};
+
+	for (var key in dictionary){
+		for (var r = 0; r<vpm.length;r++){
+			if (key.split("=")[0] == vpm[r].vpm){
+				test.push({
+					"ubicacion" : vpm[r].ubicacion.replace(/[:_]/g, " ").toUpperCase(),
+					"estado" : dictionary[key].replace(/_/g, " ").toUpperCase(),
+					"alert" : alerts[dictionary[key]]
+				})
+				TestSave = new db({
+					"ubicacion":  vpm[r].ubicacion,
+					"estado": dictionary[key],
+				}).save()
+			}
+		}
+	}
+
+	socket.emit('msg', {'msg': test});
+	socket.emit('msgWrite', msgWrite);
+
+	})
+
+}, 15000);
 
 });
